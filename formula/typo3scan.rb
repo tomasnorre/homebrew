1d97b8c11c8fd329369b274b0fa9948037015533
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2020-04-06 16:35:31 +0200

    [TASK] Update typo3scan to 1.6.5

--- formula/typo3scan.rb
+++ formula/typo3scan.rb
@@ -1,9 +1,9 @@
 class Typo3scan < Formula
   desc "TYPO3 Scanner"
   homepage "https://github.com/Tuurlijk/typo3scan"
-  url "https://github.com/Tuurlijk/typo3scan/releases/download/1.6.4/typo3scan.phar"
-  sha256 "b2b8df0f9ed6aa6d818499a15bbc2ca757b9d0964698bf95dbeb38d75b80e615"
-  version "1.6.4"
+  url "https://github.com/Tuurlijk/typo3scan/releases/download/1.6.5/typo3scan.phar"
+  sha256 "0636a8ddc45fb3254046ef1ec57851fb5d72561305dd34c1f291abc2dd8610ff"
+  version "1.6.5"
 
   bottle :unneeded
 
